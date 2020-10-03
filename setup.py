1b0c49826420f4961abb1b569cd1823325577b0e
Denis Vasilik <contact@denisvasilik.com>
2020-10-03 21:48:11 +0000

    Add setup description

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__tag__ = ""
+__build__ = 0
+__version__ = "{}".format(__tag__)
+
+setuptools.setup(
+    name="padding-oracle",
+    version=__version__,
+    author="Denis Vasilìk",
+    author_email="security@denisvasilik.com",
+    url="https://www.denisvasilik.com",
+    project_urls={
+        "Bug Tracker": "https://github.com/denisvasilik/padding-oracle/issues/",
+        "Source Code": "https://github.com/denisvasilik/padding-oracle/",
+    },
+    description="Padding Oracle Attack Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+    dependency_links=[],
+    package_dir={"padding_oracle": "padding_oracle"},
+    package_data={},
+    data_files=[("", ["CHANGELOG.md"])],
+    setup_requires=[],
+    install_requires=[],
+)
